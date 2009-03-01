e3afcfa66b027b9dbc8e8fafbc34e116b6c1ff68
Nigel Kukard <nkukard@lbsd.net>
2009-03-01 19:09:05 +0000

    * Updated copyright

--- smradius/modules/userdb/mod_userdb_sql.pm
+++ smradius/modules/userdb/mod_userdb_sql.pm
@@ -1,6 +1,6 @@
 # SQL user database support
 #
-# Copyright (C) 2008, AllWorldIT
+# Copyright (C) 2008-2009, AllWorldIT
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
