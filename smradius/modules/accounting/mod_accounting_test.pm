48d083f46ed1d246493272dcd51913d007b6b43e
Robert Anderson <randerson@lbsd.net>
2009-07-14 07:46:19 +0000

    NK: Fixed package names

--- smradius/modules/accounting/mod_accounting_test.pm
+++ smradius/modules/accounting/mod_accounting_test.pm
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-package mod_accounting_test;
+package smradius::modules::accounting::mod_accounting_test;
 
 use strict;
 use warnings;
