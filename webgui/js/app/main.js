3f203f6f18e36ce824cf1f94e9975a8cb459ad80
Robert Anderson <randerson@lbsd.net>
2009-08-06 07:03:09 +0000

    Removed deprecated gridfilters code Load correct files for gridfilters

--- webgui/js/app/main.js
+++ webgui/js/app/main.js
@@ -16,15 +16,6 @@ Ext.onReady(function(){
 	// Turn on validation errors beside the field globally
 	Ext.form.Field.prototype.msgTarget = 'side';
 
-	// Range menu items, used on GridFilter
-        Ext.menu.RangeMenu.prototype.icons = {
-		gt: 'resources/extjs/images/greater_then.png',
-		lt: 'resources/extjs/images/less_then.png',
-		eq: 'resources/extjs/images/equals.png'
-        };
-        Ext.grid.filter.StringFilter.prototype.icon = 'resources/extjs/images/find.png';
-
-
 	// Turn off the loading icon
 	var hideMask = function () {
 		Ext.get('loading').remove();
