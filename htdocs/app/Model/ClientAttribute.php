1e696e407a5c78c0c10e67a0186816f601b01c07
Nigel Kukard <nkukard@lbsd.net>
2014-06-24 07:38:18 +0000

    Fixed syntax and identation

--- htdocs/app/Model/ClientAttribute.php
+++ htdocs/app/Model/ClientAttribute.php
@@ -8,6 +8,20 @@ class ClientAttribute extends AppModel
 {
 	public $useTable = 'client_attributes';
 
-	//Validating form controllers.
-	public $validate = array('Name' => array('required' => array('rule' => array('notEmpty'),'message' => 'Please enter value')), 'Value' => array('required' => array('rule' => array('notEmpty'),'message' => 'Please enter value')));
+	// Validating form controllers.
+	public $validate = array(
+		'Name' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'Please enter value')
+			),
+		'Value' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'Please enter value'
+			)
+		)
+	);
 }
+
+// vim: ts=4
