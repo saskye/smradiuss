4bcce9087783c9e718b40de042e4bef63f776aa0
Nigel Kukard <nkukard@lbsd.net>
2014-06-24 07:36:48 +0000

    Fixed identation and syntax

--- htdocs/app/Model/Client.php
+++ htdocs/app/Model/Client.php
@@ -8,6 +8,21 @@ class Client extends AppModel
 {
 	public $useTable = 'clients';
 
-	//Validating form controllers.
-	public $validate = array('Name' => array('required' => array('rule' => array('notEmpty'),'message' => 'Please enter value.')),'AccessList' => array('required' => array('rule' => array('notEmpty'),'message' => 'Please enter value.')));
+	// Validating form controllers
+	public $validate = array(
+		'Name' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'Please enter value.'
+			)
+		),
+		'AccessList' => array(
+			'required' => array(
+				'rule' => array('notEmpty'),
+				'message' => 'Please enter value.'
+			)
+		)
+	);
 }
+
+// vim: ts=4
