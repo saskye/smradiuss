cacc8966bc20b5dbf6884189f396a914e9dd1a68
shail <ssolanki@allworldit.com>
2014-07-09 06:06:51 +0000

    All fixed on Controller/GroupsController.php in 'l-new-mr12' branch.

--- htdocs/app/Controller/GroupsController.php
+++ htdocs/app/Controller/GroupsController.php
@@ -23,28 +23,34 @@
  *
  * @class GroupsController
  *
- * @brief This class manage the groups.
+ * @brief This class manages the groups.
  */
-class GroupsController extends AppController {
+class GroupsController extends AppController
+{
 
 	/**
 	 * @method index
 	 * This method is used for Showing group list with pagination.
 	 */
-	public function index(){
+	public function index()
+	{
 		$this->Group->recursive = -1;
 		$this->paginate = array('limit' => PAGINATION_LIMIT );
 		$groups = $this->paginate();
 		$this->set('groups', $groups);
 	}
 
+
+
 	/**
 	 * @method add
 	 * This method is used to add groups.
 	 */
-	public function add(){
-		if ($this->request->is('post')){
+	public function add()
+	{
+		if ($this->request->is('post')) {
 			$this->Group->set($this->request->data);
+
 			// Validating entered data.
 			if ($this->Group->validates()) {
 				// Saving data.
@@ -56,23 +62,27 @@ class GroupsController extends AppController {
 		}
 	}
 
+
+
 	/**
 	 * @method edit
 	 * @param $id
 	 * This method is used to edit groups.
 	 */
-	public function edit($id){
+	public function edit($id)
+	{
 		$group = $this->Group->findById($id);
 		$this->set('group', $group);
+
 		// Checking submit button is clicked or not
-		if ($this->request->is('post')){
+		if ($this->request->is('post')) {
 			$this->Group->set($this->request->data);
+
 			// Validating submitted data.
 			if ($this->Group->validates()) {
 				$this->Group->id = $id;
 				$this->Group->save($this->request->data);
 				$this->Session->setFlash(__('Group is edited succefully!', true), 'flash_success');
-
 				// For reload page to reflect change in data
 				$group = $this->Group->findById($id);
 				$this->set('group', $group);
@@ -82,14 +92,17 @@ class GroupsController extends AppController {
 		}
 	}
 
+
+
 	/**
 	 * @method remove
 	 * @param $id
-	 * This method is used to delete group.
+	 * This method is used to delete groups.
 	 */
-	public function remove($id){
+	public function remove($id)
+	{
 		// Deleting
-		if($this->Group->delete($id)){
+		if ($this->Group->delete($id)) {
 			// Redirected to index function.
 			$this->redirect('/groups/index');
 			$this->Session->setFlash(__('Group is removed succefully!', true), 'flash_success');
