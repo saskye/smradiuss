c2ae59ccd0c43d43cfb49e24fa85e843dd3a3f29
Charl Mert <cmert@lbsd.net>
2014-07-18 16:18:49 +0200

    Updated SQL formatting 22nd MR

--- htdocs/app/Model/WispUser.php
+++ htdocs/app/Model/WispUser.php
@@ -227,7 +227,7 @@ class WispUser extends AppModel
 	//Delete group.
 	public function deleteUserGroup($userId)
 	{
-		 $res = $this->query("delete from users_to_groups where UserID =".$userId);
+		 $res = $this->query("DELETE FROM users_to_groups WHERE UserID = ?", array($userId));
 	}
 
 
