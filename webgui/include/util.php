6eedd983964d62b8d1828a01f899f6fcad91a3ce
Wepongo <wembo.pongo@gmail.com>
2015-06-24 11:17:07 +0000

    copyright date update.

--- webgui/include/util.php
+++ webgui/include/util.php
@@ -1,6 +1,6 @@
 <?php
 # Utility functions
-# Copyright (C) 2010-2011, AllWorldIT
+# Copyright (C) 2010-2015, AllWorldIT
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
